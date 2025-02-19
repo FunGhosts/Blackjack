8b55f64f7a5e111d6999b8030f06e2b8f4a733bc
FunGhosts <anna.mogyorosi123@gmail.com>
2025-02-19 17:32:28 +0100

    Add files via upload
    This is the updated vesion of the game after a quick bug fixing session, now the ace works properly and the game actually stops one you win or fold. If anyone has any advice on how to make it more efficient please feel free to share <3

--- black_jack.py
+++ black_jack.py
@@ -2,22 +2,29 @@ import random
 deck_type=  ["Hearts", "Spades", "Clubs", "Diamonds"]
 deck_value= ["Ace","2","3","4","5","6","7","8","9","Jack","Queen","King" ]
 
-def pull(a):
+def pull(a,val):
     if a > 1:
+        global card1_value
         card1_value= random.randint(0,11)
-        card1_type= random.randint(0,3)
+        card1_type= random.randint(0,3) 
         card1= deck_value[card1_value ] + " of " + deck_type [card1_type ] 
+        card1_value = card1_value + 1
         if card1_value>10:
             card1_value=10
+        elif card1_value==1 and val<10:
+          card1_value= 11
         return card1,card1_value
 
+
 def check(a,dealer):
-    if a>=21 or (a<dealer and dealer<21):
-          return False
-    elif a<21 and a>dealer:
-        return True
+    global status
+    if a>21 or (a<dealer and dealer<21):
+          status= "lose"
+    elif a<=21 and a>dealer:
+        status= "win"
     elif dealer>21:
-         return True
+         status= "win"
+    return status
 
 def bidding_level(bid):
      if bid> 0 and bid<100:
@@ -27,7 +34,7 @@ def bidding_level(bid):
      elif bid>500:
           return "high level bid"
 
-print(pull(2)[0])
+print(pull(2,0)[0])
 
 cards=[]
 
@@ -38,18 +45,24 @@ end= False
 val = 0
 bid= 0 
 while start == "yes" or "y" and end==False:
-    bid=input("What is your starting bid?")
+    #bid=input("What is your starting bid?")
     if first== True:
-         dealer= pull(2)[1]
-         cards.append(pull(2)[0])
-         val =val + pull(2)[1]
-    cards.append(pull(2)[0])
-    dealer= dealer + pull(2)[1]
+         dealer= pull(2,val)[1]
+         cards.append(pull(2,val)[0])
+         val =val + card1_value
+         print(card1_value)
+         print(val)
+    cards.append(pull(2,val)[0])
+    val =val + card1_value
+    print(val)
+
+    dealer= dealer + pull(2,val)[1]
     print(dealer)
     print(cards)
-    
+    print(val)
+    check(val, dealer)
 
-    for i in range(count):
+    for i in range(count) and status :
             if check(val,dealer)== False and first== False:
                 print("lose")
             elif check(val,dealer)== True and first== False:
@@ -59,10 +72,20 @@ while start == "yes" or "y" and end==False:
             if another == "y":
                  print(check(val,dealer))
                  break
-            elif another == "n":
+            elif another == "n" :
                  print(check(val,dealer))
                  end=True
+                 start= "n"
+                 count= -1
                  break
+            elif another == "fold":
+               end= True 
+               count= -1
+               fold = True
+               print("folded")
+               start= "n"
+               break
+               
             count= count + 1 
     print()
     
